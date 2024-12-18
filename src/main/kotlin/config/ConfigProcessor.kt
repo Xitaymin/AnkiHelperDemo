b15c09fd9cb2332fa53783efa2a79ed26a559a43
Yaroslav Barashenko <y.barashenko@gmail.com>
2024-12-18 23:53:03 +0200

    TDD Demo Anki Helper - Partial config fix

--- src/main/kotlin/config/ConfigProcessor.kt
+++ src/main/kotlin/config/ConfigProcessor.kt
@@ -1,22 +1,34 @@
-package org.demotdd.config
-
+package config
 import java.util.Properties
+import java.io.FileOutputStream
 
 // Використовуйте цей клас для роботи з конфігураційним файлом
 class ConfigProcessor {
     private val properties = Properties()
+    private val configFileName = "config.properties"
 
     init {
-        val inputStream = this::class.java.classLoader.getResourceAsStream("config.properties")
-        inputStream?.use { properties.load(it) } ?: throw IllegalArgumentException("config.properties not found")
+        val inputStream = this::class.java.classLoader.getResourceAsStream(configFileName)
+        inputStream?.use { properties.load(it) } ?: throw IllegalArgumentException("$configFileName not found")
     }
 
-    fun getApiKey(): String {
-        return properties.getProperty("deepl.api.key")
+    fun getApiKey(): String? = try {
+        properties.getProperty("deepl.api.key")
+    } catch (e: Exception) {
+        null
     }
 
-    fun setApiKey(key: String): Unit {
+    fun setApiKey(key: String) {
         properties.setProperty("deepl.api.key", key)
+        saveProperties()
     }
 
+    private fun saveProperties() {
+        val fileUrl = this::class.java.classLoader.getResource(configFileName)
+            ?: throw IllegalArgumentException("$configFileName not found")
+
+        FileOutputStream(fileUrl.path).use { outputStream ->
+            properties.store(outputStream, "Updated deepl.api.key")
+        }
+    }
 }
